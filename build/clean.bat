4d9bca57bf2b711215be578e22fe7de636a459b5
Silas Reinagel <silas.reinagel@gmail.com>
2017-11-06 00:02:36 -0700

    Fixes

---
+++ build/clean.bat
@@ -0,0 +1,12 @@
+cd LegendaryFrailty/www
+rm *.psd
+rm *.png
+rmdir build /s /q
+rmdir temp /s /q
+rmdir utils /s /q
+rmdir characters /s /q
+rmdir screenshots /s /q
+rm .gitignore
+mkdir save
+cd..
+cd..

1e05a99a7e08bed9600e07672a21dffcd4174cc9
noah reinagel <noah.r9@gmail.com>
2017-11-28 21:45:33 -0700

    Add character selection skip plugin

---
+++ js/plugins/ED_SkipMenuCharacterSelection.js
@@ -0,0 +1,7 @@
+Scene_Menu.prototype.commandPersonal = function() {
+    this._statusWindow.setFormationMode(false);
+    this._statusWindow.selectLast();
+    this._statusWindow.activate();
+    this.onPersonalOk();
+    this._statusWindow.show();
+};

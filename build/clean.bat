b4b677327cb0424f22628841ff881996b6f58c29
Silas Reinagel <silas.reinagel@gmail.com>
2017-11-30 21:27:28 -0700

    Cleaning up more files

--- build/clean.bat
+++ build/clean.bat
@@ -1,12 +1,15 @@
 cd LegendaryFrailty/www
 rm *.psd
 rm *.png
-rm *Aclonica-LICENSE.txt
+rm *fonts/Aclonica-LICENSE.txt
 rmdir build /s /q
 rmdir temp /s /q
 rmdir utils /s /q
 rmdir characters /s /q
 rmdir screenshots /s /q
+rmdir img/enemies /s /q
+rmdir img/parallaxes /s /q
+rmdir img/portraits /s /q
 rm .gitignore
 mkdir save
 cd..
